42e450ebefd86b6f5891a9d896f1dce5cc7054d3
Valeryia Hrabnitskaya <Valera17041997@mail.ru>
2019-11-29 00:09:57 +0300

    na2 add vote

--- Voting/script.js
+++ Voting/script.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
 
 var path = require('path');
 
@@ -6,34 +8,54 @@ const webserver = express();
 
 const port = 7480;
 
+webserver.use(express.json());
+webserver.use(bodyParser.text());
+
+webserver.options('/vote', (req, res) => {
+
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+    res.send(""); // сам ответ на preflight-запрос может быть пустым
+});
 
 webserver.get('/main-page', (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 webserver.get('/stat', (req, res) => {
-    res.send({
-        data: [
-            { id: 1, value: 1 },
-            { id: 2, value: 2 },
-            { id: 3, value: 3 },
-            { id: 4, value: 4 },
-            { id: 5, value: 5 }
-        ]
-    });
+    let fileContent = JSON.parse(fs.readFileSync("votes.txt", "utf8"));
+    res.send(fileContent);
 });
 
 webserver.get('/variants', (req, res) => {
-    res.send({
-        data: [
-            { id: 1, value: 1 },
-            { id: 2, value: 2 },
-            { id: 3, value: 3 },
-            { id: 4, value: 4 },
-            { id: 5, value: 5 }
-        ]
-    });
+    let fileContent = JSON.parse(fs.readFileSync("votes.txt", "utf8"));
+    res.send(fileContent);
 });
+
+webserver.post('/vote', (req, res) => {
+
+    const contentType = req.headers['content-type'];
+
+    if (contentType === "text/plain") {
+        setAndGetDataFromFile(req.body);
+        res.status(200).type('text/plain');
+        res.send();
+    }
+});
+
+function setAndGetDataFromFile(value) {
+    const intValue = parseInt(value);
+    let fileContent = JSON.parse(fs.readFileSync("votes.txt", "utf8"));
+    var newFileContent = fileContent.data.map((item) => {
+        if (item.id === intValue) {
+            item.value += 1;
+        }
+        return item;
+    });
+    console.log(JSON.stringify({"data": newFileContent}))
+    fs.writeFileSync("votes.txt", JSON.stringify({"data": newFileContent}));
+}
 
 webserver.listen(port, () => {
     console.log("web server running on port " + port);
