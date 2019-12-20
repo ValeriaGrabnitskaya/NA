aefc40776122762f64c35af8162f3a182bd797e4
Valeryia Hrabnitskaya <VHrabnitskaya@iba.by>
2019-12-20 18:48:25 +0300

    na5 add table, add run

--- Postman/public/index.js
+++ Postman/public/index.js
@@ -1,3 +1,31 @@
+function firstPageLoad() {
+    getRequestsList();
+}
+
+async function getRequestsList() {
+    const fetchOptions = {
+        method: "get",
+    };
+    const response = await fetch('/requests-list', fetchOptions);
+    const data = await response.json();
+    buildRequestBlock(data);
+}
+
+function resetForm() {
+    document.getElementById('method').value = '',
+    document.getElementById('url').value = '',
+    document.getElementById('keyParam1').value = '',
+    document.getElementById('valueParam1').value = '',
+    document.getElementById('keyParam2').value = '',
+    document.getElementById('valueParam2').value = '',
+    document.getElementById('contentType').value = '',
+    document.getElementById('keyBody1').value = '',
+    document.getElementById('valueBody1').value = '',
+    document.getElementById('keyBody2').value = '',
+    document.getElementById('valueBody2').value = ''
+}
+
+
 async function submitForm() {
     const fetchOptions = {
         method: "post",
@@ -5,13 +33,15 @@ async function submitForm() {
     };
     console.log(JSON.stringify(this.getFormData()))
     const response = await fetch('/saveRequestData', fetchOptions);
-    const data = await response.json();
-    buildRequestBlock(data);
+    if(response.status = 200) {
+        getRequestsList();
+        resetForm();
+    }
 }
 
 function getFormData() {
     return {
-        method: document.getElementById('method').value,
+        methodId: document.getElementById('method').value,
         url: document.getElementById('url').value,
         keyParam1: document.getElementById('keyParam1').value,
         valueParam1: document.getElementById('valueParam1').value,
@@ -28,8 +58,17 @@ function getFormData() {
 function buildRequestBlock(response) {
     let requestBlock = '';
     response.data.forEach((item, index) => {
-        requestBlock += `<div><div>${item.url}</div><div>${item.method}</div><div><input type="button" click="executeService(${item.id})"/></div></div>`
+        requestBlock += `<tr><td>${item.methodName}</td><td>${item.url}</td><td><input type="button" value="Отправить" onclick="executeService(${item.id})"/></td></tr>`
     })
     var requestDiv = document.getElementById('requestBlock');
     requestDiv.innerHTML = requestBlock;
+}
+
+async function executeService(requestId) {
+    console.log(requestId);
+    const fetchOptions = {
+        method: "post",
+        body: JSON.stringify({requestId: requestId})
+    };
+    const response = await fetch('/run-request', fetchOptions);
 }
--- Postman/script.js
+++ Postman/script.js
@@ -8,7 +8,8 @@ const webserver = express();
 
 const port = 7480;
 
-let key = 0;
+const POST = 1;
+const GET = 2;
 
 webserver.use(express.json());
 webserver.use(bodyParser.text());
@@ -22,18 +23,37 @@ webserver.get('/main-page', (req, res) => {
     res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
+webserver.get('/requests-list', (req, res) => {
+    let fileContent = JSON.parse(fs.readFileSync("requsts-data.json", "utf8"));
+    res.send(fileContent);
+});
+
 webserver.post('/saveRequestData', (req, res) => {
     let fileContent = JSON.parse(fs.readFileSync("requsts-data.json", "utf8"));
     var parsedData = JSON.parse(req.body);
     fileContent.data.push(setId(parsedData))
     fs.writeFileSync("requsts-data.json", JSON.stringify(fileContent));
-    res.send(fileContent);
+    res.send(200);
 });
 
 function setId(parsedData) {
-    parsedData.id = key;
-    key += 1;
+    parsedData.id = Math.random();
+    parsedData.methodName = +parsedData.methodId === POST ? 'POST' : 'GET';
     return parsedData;
+}
+
+webserver.post('/run-request', async (req, res) => { 
+    let selectedRequest = getRequestById(req.body.requestId)[0];
+
+    const proxy_response=await fetch(`http://localhost:7481/${selectedRequest.url}`);
+    const proxy_json=await proxy_response.json();
+console.log(proxy_json)
+    // res.send(proxy_text);
+});
+
+function getRequestById(requestId) {
+    let fileContent = JSON.parse(fs.readFileSync("requsts-data.json", "utf8"));
+    return fileContent.data.filter((item) => item.id === requestId);
 }
 
 webserver.listen(port, () => {
