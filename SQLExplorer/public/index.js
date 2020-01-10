1794a0de5c89306ceeb272c545a75e4548b7d1fd
Valeryia Hrabnitskaya <VHrabnitskaya@iba.by>
2020-01-10 23:29:15 +0300

    na7 add sql explorer

---
+++ SQLExplorer/public/index.js
@@ -0,0 +1,102 @@
+async function getDatabases() {
+    const fetchOptions = {
+        headers: {
+            'Accept': 'application/json'
+        }
+    };
+
+    const response = await fetch('/get-databases', fetchOptions);
+    if (response.ok) {
+        const data = await response.json();
+        console.log(data)
+
+        let res = `<option value="" disabled="disabled" selected="selected">Выберите базу для работы</option>`;
+        data.forEach((row) => {
+            res += `<option value="${row.Database}">${row.Database}</option>`;
+        });
+        document.getElementById('databases').innerHTML = res;
+    }
+    else {
+        document.getElementById('databases').innerHTML = '';
+    }
+}
+
+async function onSelectDatabase() {
+    const fetchOptions = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ database: document.getElementById('databases').value })
+    };
+
+    const response = await fetch('/use-database', fetchOptions);
+
+    if (response.ok) {
+        const data = await response.json();
+
+        addLog(`USE ${data.database};`);
+    } else {
+        addLog(`<p>ошибка в подключении к базе ${data.database};</p>`);
+    }
+}
+
+async function execute() {
+    const fetchOptions = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ sql: document.getElementById('sqlRequest').value })
+    };
+
+    const response = await fetch('/execute-sql', fetchOptions);
+    if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+        console.log(data.fields);
+
+        // header
+        if (data.fields && data.results) {
+            let table = '<table>';
+
+            let header = '<tr>';
+            data.fields.forEach((row) => {
+                header += `<th>${row.fieldName}</th>`;
+            });
+            header += '</tr>';
+            table += header;
+
+            // body
+            let body = '';
+            data.results.forEach((row) => {
+                body += '<tr>';
+                for (let [key, value] of Object.entries(row)) {
+                    body += `<td>${value}</td>`;
+                }
+                body += '</tr>';
+            });
+            table += body + '</table>';
+
+            document.getElementById('sqlResponse').innerHTML = table;
+        }
+
+        addLog(`Количество выбранных строк: ${data.rowCount};`);
+    } else {
+        addLog(`ошибка чтения данны`)
+    }
+}
+
+function addLog(text) {
+    var lastRow = document.querySelector('div#actionOutput');
+    var elem = document.createElement("p");
+    var elemText = document.createTextNode(`${text}`);
+    elem.appendChild(elemText);
+
+    if (lastRow) {
+        lastRow.appendChild(elem);
+    } else {
+        document.getElementById('actionOutput').innerHTML = `<p>${text}</p>`;
+    }
+}
+
---
+++ SQLExplorer/script.js
@@ -0,0 +1,111 @@
+﻿const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const mysql = require("mysql");
+
+let connectionConfig = {
+    host: 'localhost',  // на каком компьютере расположена база данных
+    user: 'root',   // каким пользователем подключаемся (на учебном сервере - "root")
+    password: 'Hrab-123',   // каким паролем подключаемся (на учебном сервере - "1234")
+    // database: 'smart_people' // к какой базе данных подключаемся
+};
+
+const webserver = express();
+
+const port = 7481;
+
+webserver.use(bodyParser.json());
+
+webserver.use(
+    express.static(path.resolve(__dirname, "public"))
+);
+
+webserver.get('/main-page', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+webserver.get('/get-databases', (req, res) => {
+    console.log('/get-databases');
+    let connection = null;
+    try {
+        connection = mysql.createConnection(connectionConfig);
+        connection.connect();
+        connection.query(
+            'SHOW DATABASES' 
+            , (error, results, fields) => {
+                if (error) {
+                    res.status(500).end();
+                }
+                else {
+                    res.send(results);
+                }
+                connection.end();
+            });
+    }
+    catch (error) {
+        res.status(500).end();
+        if (connection)
+            connection.end();
+    }
+});
+
+webserver.post('/use-database', (req, res) => {
+    console.log('/use-database');
+    let connection = null;
+    try {
+        connection = mysql.createConnection(connectionConfig);
+        connection.connect();
+        connection.query(
+            `USE ${req.body.database};`
+            , (error, results, fields) => {
+                if (error) {
+                    res.status(500).end();
+                }
+                else {
+                    connectionConfig.database = req.body.database;
+                    res.send({database: req.body.database});
+                }
+                connection.end();
+            });
+    }
+    catch (error) {
+        res.status(500).end();
+        if (connection)
+            connection.end();
+    }
+});
+
+webserver.post('/execute-sql', (req, res) => {
+    console.log('/execute-sql');
+    let connection = null;
+    try {
+        connection = mysql.createConnection(connectionConfig);
+        connection.connect();
+        connection.query(
+            req.body.sql
+            , (error, results, fields) => {
+                if (error) {
+                    res.status(500).end();
+                }
+                else {
+                    let mapFields = fields.map(row => ({ fieldName: row.name }));
+                    
+                    res.send({
+                        results: results,
+                        fields: mapFields,
+                        rowCount: results.length
+                    });
+                }
+                connection.end();
+            });
+    }
+    catch (error) {
+        res.status(500).end();
+        if (connection)
+            connection.end();
+    }
+});
+
+webserver.listen(port, () => {
+    console.log("web server running on port " + port);
+});
