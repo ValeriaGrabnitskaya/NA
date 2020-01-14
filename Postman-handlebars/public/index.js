45744ad22973464e03da73bfe9a1f6a0804cd00b
Valeryia Hrabnitskaya <VHrabnitskaya@iba.by>
2020-01-14 13:13:05 +0300

    na8 refactor hbs and front script

--- Postman-handlebars/public/index.js
+++ Postman-handlebars/public/index.js
@@ -1,5 +1,3 @@
-import { setFormData, getFormData, setErrorBlock } from './mappers/data-mapper.js';
-
 function resetPage(formName) {
     resetForm(formName);
     setErrorBlock('');
@@ -45,4 +43,47 @@ async function executeService(requestId) {
     const response = await fetch('/run-request', fetchOptions);
     const data = await response.json();
     setFormData('resForm', data);
+}
+
+function setFormData(formName, response) {
+    let formParams = getFormData(formName);
+
+    let searchFormElements = document.forms[formName].elements;
+
+    for (let resParam in response) {
+        if (formParams.hasOwnProperty(resParam)) {
+            if (resParam === 'resHeaders') {
+                searchFormElements[resParam].value = mapResHeaders(response[resParam]);
+            } else {
+                searchFormElements[resParam].value = response[resParam];
+            }
+        }
+    }
+}
+
+function getFormData(formName) {
+    let searchFormElements = document.forms[formName].elements;
+    let params = {};
+
+    for (var i = 0; i < searchFormElements.length; i++) {
+        if (searchFormElements[i].name) {
+            params = { ...params, [searchFormElements[i].name]: searchFormElements[searchFormElements[i].name].value }
+        }
+    }
+    return params;
+}
+
+function mapResHeaders(resHeaders) {
+    let str = '';
+    for (let [key, value] of Object.entries(resHeaders)) {
+        str += `${key}: ${value};\n\n`;
+    }
+    return str;
+}
+
+function setErrorBlock(message) {
+    var errorsContainer = document.getElementById('errors');
+    if (message) {
+        errorsContainer.innerHTML = '<li style="color: red">' + message + '</li>';
+    }
 }
